25020c45c6f8ffa2a617d32c86d2db0cbd157dff
violetabsence <violetabsence@gmail.com>
2021-09-27 04:17:19 +0700

    Change storage to MS Sql server.

---
+++ ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using TodoApiDTO.Repository;
+
+namespace TodoApiDTO
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureMySqlContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("TodoItemsDatabase");
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentNullException("connectionString", "Connection string should not be empty.");
+            }
+            services.AddDbContext<RepositoryContext>(o => o.UseSqlServer(connectionString));
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -27,8 +27,7 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(opt =>
-               opt.UseInMemoryDatabase("TodoList"));
+            services.ConfigureMySqlContext(Configuration);
             services.AddControllers();
             services.AddSwaggerGen();
         }
